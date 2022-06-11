0f1ebf682af0597705e08157e3fe3957dc727456
Istvan <olivaistvan@gmail.com>
2022-06-11 13:59:01 -0300

    Req02: criando rotas products e sales

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 const express = require('express');
 
+const productsController = require('./controllers/productController');
+const salesController = require('./controllers/salesController');
+
 const app = express();
+app.use(express.json());
 
 // não remova esse endpoint, é para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
 
-// não remova essa exportação, é para o avaliador funcionar
-// você pode registrar suas rotas normalmente, como o exemplo acima
-// você deve usar o arquivo index.js para executar sua aplicação 
+app.get('/products', productsController.getAll);
+app.get('/products/:id', productsController.getProduct);
+
+app.get('/sales', salesController.getAll);
+app.get('/sales/:id', salesController.getSaleById);
+ 
 module.exports = app;
