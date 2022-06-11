25ef169200420027c8441c7aeb1cb75aebe327ae
Istvan <olivaistvan@gmail.com>
2022-06-11 13:56:39 -0300

    Req02: model - sales

---
+++ models/salesModel.js
@@ -0,0 +1,25 @@
+const connection = require('./connection');
+
+const getAll = async () => {
+    const query = `
+    SELECT * FROM StoreManager.sales as a
+    JOIN StoreManager.sales_products as sp
+    WHERE a.id = sp.sale_id`;
+    const [sales] = await connection.execute(query);
+    return sales;
+};
+
+const getSale = async (id) => {
+    const query = `
+    SELECT * FROM StoreManager.sales as s
+    JOIN StoreManager.sales_products as sp
+    ON s.id = sp.sale_id
+    WHERE s.id = ?`;
+    const [sale] = await connection.execute(query, [id]);
+    return sale;
+};
+
+module.exports = {
+    getAll,
+    getSale,
+};
