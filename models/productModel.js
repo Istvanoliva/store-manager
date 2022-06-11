c31548cf52c129f71ab85f410fb2e5b1243943ef
Istvan <olivaistvan@gmail.com>
2022-06-11 13:54:21 -0300

    Req02: model - product

---
+++ models/productModel.js
@@ -0,0 +1,18 @@
+const connection = require('./connection');
+
+const getAll = async () => {
+    const query = 'SELECT * FROM StoreManager.products';
+    const [products] = await connection.execute(query);
+    return products;
+};
+
+const getProduct = async (id) => {
+    const query = 'SELECT * FROM StoreManager.products WHERE id = ?';
+    const [products] = await connection.execute(query, [id]);
+    return products;
+};
+
+module.exports = {
+    getAll,
+    getProduct,
+};
